0dfc33d9cf210bccfcfaddaf4e510da1bf1950e9
koga <popweb782@gmail.com>
2020-12-04 19:43:48 +0900

    カスタムディレクティブ@verifiedの追加

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -49,7 +49,8 @@
                                     <a class="nav-link" href="{{ route('register') }}">新規登録</a>
                                 </li>
                             @endif
-                        @else
+                        @endguest
+                        @verified
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     設定<span class="caret"></span>
@@ -67,7 +68,7 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
+                        @endverified
                     </ul>
                 </div>
             </div>
