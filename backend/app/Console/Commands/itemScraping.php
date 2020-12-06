6971f3ae1fd3bef661ca0e6e9d04b5cbd475667a
koga <popweb782@gmail.com>
2020-12-06 22:54:54 +0900

    定期実行の処理追加（WIP）

--- backend/app/Console/Commands/itemScraping.php
+++ backend/app/Console/Commands/itemScraping.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Goutte\Client;
+use App\Item;
 
 class itemScraping extends Command
 {
@@ -38,11 +39,17 @@ class itemScraping extends Command
      */
     public function handle()
     {
-        $client = new Client();
-        $crawler = $client->request('GET', 'https://www.amazon.co.jp/PHP%E3%83%95%E3%83%AC%E3%83%BC%E3%83%A0%E3%83%AF%E3%83%BC%E3%82%AF-Laravel-Web%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E9%96%8B%E7%99%BA-%E3%83%90%E3%83%BC%E3%82%B8%E3%83%A7%E3%83%B35-5-LTS%E5%AF%BE%E5%BF%9C/dp/4802611846/ref=pd_bxgy_img_3/357-9882718-2115317?_encoding=UTF8&pd_rd_i=4802611846&pd_rd_r=363b7d92-fb01-4010-9ff7-fea251716303&pd_rd_w=ClFc2&pd_rd_wg=9jBz2&pf_rd_p=e64b0a81-ca1b-4802-bd2c-a4b65bccc76e&pf_rd_r=4NMJR01VDTVX207JSW1D&psc=1&refRID=4NMJR01VDTVX207JSW1D');
-        $itemTitle = $crawler->filter('#title')->each(function ($title) {
-            return $title->text();
-        });
-        dd($itemTitle);
+        $items = Item::all();
+        foreach ($items as $item){
+            $client = new Client();
+            $crawler = $client->request('GET', $item->url);
+            $itemPrice = $crawler->filter('#buybox')->filter('.kindle-price')->filter('.a-size-large')->filter('span')->last()->text();
+            $intItemPrice = (int)str_replace(',', '',preg_replace('/￥/','',$itemPrice));
+            if($item->registration_price > $intItemPrice){
+                $item->status = 1;
+                $item->current_price = $intItemPrice;
+                $item->save();
+            }
+        }
     }
 }
--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -51,6 +51,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('mypage.index') }}">マイリスト</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     設定<span class="caret"></span>
