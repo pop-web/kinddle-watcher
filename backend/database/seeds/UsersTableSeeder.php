ae2cd414c7e665ba5b52bd7868798e9da2a082af
koga <popweb782@gmail.com>
2020-12-17 23:32:54 +0900

    databaseの整理

--- backend/database/seeds/UsersTableSeeder.php
+++ backend/database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
             'password' => bcrypt('test'),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
+            'status' => 1,
         ]);
     }
 }
