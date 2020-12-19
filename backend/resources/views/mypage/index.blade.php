3c9842cf363d399e5087b2119279f8921ba95b94
koga yusuke <popweb782@gmail.com>
2020-12-19 20:56:45 +0900

    デザインの修正

--- backend/resources/views/mypage/index.blade.php
+++ backend/resources/views/mypage/index.blade.php
@@ -57,35 +57,35 @@
         </div>
         @endforeach
     </div>
-</div>
-@endsection
-<!-- Modal -->
-<div class="modal fade" id="showItemModal" tabindex="-1" role="dialog" aria-labelledby="showItemModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <form method="POST" action="{{ route('mypage.create') }}">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="commonModalLabel">マイリスト登録</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <a href="" target="_blank"><img src="" alt="" class="w-50 mx-auto d-block"></a>
-                    <h5 class="text-center mt-3"></h5>
-                    <div class="d-flex justify-content-center mt-3">
-                        <div class="h5 bg-secondary text-white rounded-pill py-1 px-2" id="itemPrice"></div>
+    <!-- Modal -->
+    <div class="modal fade" id="showItemModal" tabindex="-1" role="dialog" aria-labelledby="showItemModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form method="POST" action="{{ route('mypage.create') }}">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="commonModalLabel">マイリスト登録</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    @csrf
-                    <input type="hidden" name="url" value="" id="hiddenUrlInput">
-                    <input type="hidden" name="title" value="" id="hiddenTitleInput">
-                    <input type="hidden" name="img_url" value="" id="hiddenImgUrl">
-                    <input type="hidden" name="registration_price" value="" id="hiddenItemPrice">
-                    <button type="submit" class="btn btn-primary">登録</button>
-                </div>
-            </form>
+                    <div class="modal-body">
+                        <a href="" target="_blank"><img src="" alt="" class="w-50 mx-auto d-block"></a>
+                        <h5 class="text-center mt-3"></h5>
+                        <div class="d-flex justify-content-center mt-3">
+                            <div class="h5 bg-secondary text-white rounded-pill py-1 px-2" id="itemPrice"></div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        @csrf
+                        <input type="hidden" name="url" value="" id="hiddenUrlInput">
+                        <input type="hidden" name="title" value="" id="hiddenTitleInput">
+                        <input type="hidden" name="img_url" value="" id="hiddenImgUrl">
+                        <input type="hidden" name="registration_price" value="" id="hiddenItemPrice">
+                        <button type="submit" class="btn btn-primary">登録</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
+@endsection
