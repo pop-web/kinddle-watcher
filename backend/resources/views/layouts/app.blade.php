c3b220d9dd8a13644450fc82ab973fbc53d091d8
koga yusuke <popweb782@gmail.com>
2020-12-21 17:03:08 +0900

    シェアボタン設置

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -81,8 +81,27 @@
         <main class="py-4">
             @yield('content')
         </main>
-        <footer class="bg-white text-center">
-            <small>&copy; {{ config('app.name', 'KinddleWatcher') }}</small>
+        <footer class="text-center">
+            <!-- Twitter -->
+            <div class="d-inline-block">
+                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+            <!-- facebook -->
+            <div class="d-inline-block">
+                <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&layout=button_count&size=small&width=103&height=20&appId" width="103" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+            </div>
+            <!-- はてな -->
+            <div class="d-inline-block">
+                <a href="https://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="basic-label-counter" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
+            </div>
+                <!-- Pocket -->
+            <div class="d-inline-block">
+                <a data-pocket-label="pocket" data-pocket-count="horizontal" class="pocket-btn" data-lang="en"></a>
+                <script type="text/javascript">!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pocket-btn-js");</script>
+            </div>
+            <div class="bg-white">
+                <small>&copy; {{ config('app.name', 'KinddleWatcher') }}</small>
+            </div>
         </footer>
     </div>
 </body>
