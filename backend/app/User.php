68ffe6c2b4dfad57460d08dbc3e7a5dc0ea327f5
koga yusuke <popweb782@gmail.com>
2021-01-12 08:10:46 +0900

    fixed

--- backend/app/User.php
+++ backend/app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','email_verified', 'email_verify_token'
+        'name', 'email', 'password','email_verified', 'token' , 'email_verify_token'
     ];
 
     /**
