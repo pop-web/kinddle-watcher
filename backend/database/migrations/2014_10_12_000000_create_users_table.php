824222edae2557d0579575a9aa124e9b09390f2f
koga yusuke <popweb782@gmail.com>
2021-01-23 15:15:18 +0900

    add migration comments

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->string('token')->nullable();
-            $table->rememberToken();
+            $table->bigIncrements('id')->comment("ID");
+            $table->string('email')->unique()->comment("E-Mail");
+            $table->timestamp('email_verified_at')->nullable()->comment("メール認証日時");
+            $table->string('password')->nullable()->comment("パスワード");
+            $table->string('token')->nullable()->comment("TwitterOAuthトークン");;
+            $table->rememberToken()->comment("パスワードリセットトークン");;
             $table->timestamps();
         });
     }
