a6d7a771c78197f0b0f37d890a3c427d47a8a1c8
koga yusuke <popweb782@gmail.com>
2021-01-29 13:40:05 +0900

    fixed Google analytics

--- backend/resources/views/gtm2.blade.php
+++ backend/resources/views/gtm2.blade.php
@@ -1,5 +1,4 @@
-gtm.blade.php
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FRMTSV"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKGM547"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
